60f25345db6d2796cc2c8ee9c543e1c46ae66190
yellowpad <zayneabraham1@gmail.com>
2018-10-07 20:56:49 -0400

    part 2

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 class App extends React.Component {
 	render () {
 		return (
-			<div>Hello World</div>
+			<div>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />					
+					<Route exact path="/contact" component={Contact} />	
+				</Switch>
+			</div>
 		)
 	}
 }
