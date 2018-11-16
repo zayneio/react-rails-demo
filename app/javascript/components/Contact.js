636d7d9f1d160edcfc15832b9cb9835faeb4a2bb
zayneio <hello@zayne.io>
2018-11-16 13:10:05 -0500

    part 3: post requests example

--- app/javascript/components/Contact.js
+++ app/javascript/components/Contact.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import axios from 'axios'
+
+const csrfToken = document.querySelector('[name="csrf-token"]').content;
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
 
 class Contact extends React.Component {
+	handleSubmit(e){
+		let message = e.target.elements[0].value
+		axios.post('/contact', {message})
+		.then( (data)=> {
+			debugger
+		})
+	}
 	render(){
 		return(
-			<div>This is the Contact page.</div>
+			<div className="container" style={{padding: '40px 0'}}>
+				<div className="row">
+					<div className="col-md-8 offset-md-2">
+						<h1>Contact Us.</h1>
+						<form onSubmit={this.handleSubmit.bind(this)}>
+							<input type="text" name="message" className="form-control" placeholder="Send Us a Message!"/>
+							<button className="btn btn-primary" type="submit">Contact Us</button>
+						</form>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  post '/contact', to: 'pages#contact'
   match '*path', to: 'pages#index', via: :all
 end
